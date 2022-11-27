f52f84943e2084576b2b696ce711d3099d95e2ef
jonahmartin <jonahmartin.jwm@gmail.com>
2022-11-27 17:35:08 -0400

    Initial commit

---
+++ MartinLab2/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace MartinLab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnHide = new System.Windows.Forms.Button();
+            this.btnData = new System.Windows.Forms.Button();
+            this.btnPicture = new System.Windows.Forms.Button();
+            this.pictureBoxspaghetti = new System.Windows.Forms.PictureBox();
+            this.lblFavouritemeal = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxmultiplier = new System.Windows.Forms.TextBox();
+            this.textBoxnumber = new System.Windows.Forms.TextBox();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxspaghetti)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnExit);
+            this.groupBox1.Controls.Add(this.btnHide);
+            this.groupBox1.Controls.Add(this.btnData);
+            this.groupBox1.Controls.Add(this.btnPicture);
+            this.groupBox1.Controls.Add(this.pictureBoxspaghetti);
+            this.groupBox1.Controls.Add(this.lblFavouritemeal);
+            this.groupBox1.Location = new System.Drawing.Point(251, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(456, 307);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Information";
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(349, 250);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(100, 51);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnHide
+            // 
+            this.btnHide.Location = new System.Drawing.Point(235, 250);
+            this.btnHide.Name = "btnHide";
+            this.btnHide.Size = new System.Drawing.Size(100, 51);
+            this.btnHide.TabIndex = 2;
+            this.btnHide.Text = "&Hide";
+            this.btnHide.UseVisualStyleBackColor = true;
+            this.btnHide.Click += new System.EventHandler(this.btnHide_Click);
+            // 
+            // btnData
+            // 
+            this.btnData.Location = new System.Drawing.Point(121, 250);
+            this.btnData.Name = "btnData";
+            this.btnData.Size = new System.Drawing.Size(100, 51);
+            this.btnData.TabIndex = 1;
+            this.btnData.Text = "&Data";
+            this.btnData.UseVisualStyleBackColor = true;
+            this.btnData.Click += new System.EventHandler(this.btnData_Click);
+            // 
+            // btnPicture
+            // 
+            this.btnPicture.Location = new System.Drawing.Point(7, 250);
+            this.btnPicture.Name = "btnPicture";
+            this.btnPicture.Size = new System.Drawing.Size(100, 51);
+            this.btnPicture.TabIndex = 0;
+            this.btnPicture.Text = "&Picture";
+            this.btnPicture.UseVisualStyleBackColor = true;
+            this.btnPicture.Click += new System.EventHandler(this.btnPicture_Click);
+            // 
+            // pictureBoxspaghetti
+            // 
+            this.pictureBoxspaghetti.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxspaghetti.Image")));
+            this.pictureBoxspaghetti.InitialImage = null;
+            this.pictureBoxspaghetti.Location = new System.Drawing.Point(6, 22);
+            this.pictureBoxspaghetti.Name = "pictureBoxspaghetti";
+            this.pictureBoxspaghetti.Size = new System.Drawing.Size(444, 198);
+            this.pictureBoxspaghetti.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxspaghetti.TabIndex = 0;
+            this.pictureBoxspaghetti.TabStop = false;
+            this.pictureBoxspaghetti.Visible = false;
+            this.pictureBoxspaghetti.Click += new System.EventHandler(this.pictureBoxspaghetti_Click);
+            // 
+            // lblFavouritemeal
+            // 
+            this.lblFavouritemeal.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.lblFavouritemeal.Location = new System.Drawing.Point(7, 22);
+            this.lblFavouritemeal.Name = "lblFavouritemeal";
+            this.lblFavouritemeal.Size = new System.Drawing.Size(442, 198);
+            this.lblFavouritemeal.TabIndex = 1;
+            this.lblFavouritemeal.Text = "My favourite meal";
+            this.lblFavouritemeal.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lblAnswer);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.textBoxmultiplier);
+            this.groupBox2.Controls.Add(this.textBoxnumber);
+            this.groupBox2.Controls.Add(this.btnReset);
+            this.groupBox2.Controls.Add(this.btnCalculate);
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(219, 307);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Multiplier";
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblAnswer.Location = new System.Drawing.Point(86, 185);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(100, 23);
+            this.lblAnswer.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(19, 186);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Answer:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 121);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Multiplier:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Number:";
+            // 
+            // textBoxmultiplier
+            // 
+            this.textBoxmultiplier.Location = new System.Drawing.Point(86, 119);
+            this.textBoxmultiplier.Name = "textBoxmultiplier";
+            this.textBoxmultiplier.Size = new System.Drawing.Size(100, 23);
+            this.textBoxmultiplier.TabIndex = 1;
+            // 
+            // textBoxnumber
+            // 
+            this.textBoxnumber.Location = new System.Drawing.Point(86, 53);
+            this.textBoxnumber.Name = "textBoxnumber";
+            this.textBoxnumber.Size = new System.Drawing.Size(100, 23);
+            this.textBoxnumber.TabIndex = 0;
+            // 
+            // btnReset
+            // 
+            this.btnReset.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnReset.Location = new System.Drawing.Point(113, 250);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(100, 51);
+            this.btnReset.TabIndex = 3;
+            this.btnReset.Text = "&Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(6, 250);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(100, 51);
+            this.btnCalculate.TabIndex = 2;
+            this.btnCalculate.Text = "&Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnPicture;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnReset;
+            this.ClientSize = new System.Drawing.Size(719, 331);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Arial", 10F);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Lab 2 by Jonah Martin";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxspaghetti)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.PictureBox pictureBoxspaghetti;
+        private System.Windows.Forms.Label lblFavouritemeal;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnHide;
+        private System.Windows.Forms.Button btnData;
+        private System.Windows.Forms.Button btnPicture;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxmultiplier;
+        private System.Windows.Forms.TextBox textBoxnumber;
+        private System.Windows.Forms.Label lblAnswer;
+    }
+}
+
---
+++ MartinLab2/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*Name: Jonah Martin
+ * Date: October 6, 2022
+ * Description: The purpose of this form is to calculate simple multiplication problems as well as
+ * to show a picture and a description of the topic (my favourite meal) using different buttons to
+ * switch between photo and description. */
+
+namespace MartinLab2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //the purpose of the following code is to close the program using the Exit button.
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        //the purpose of the following code is to show the image of spaghetti and to hide the label.
+        private void btnPicture_Click(object sender, EventArgs e)
+        {
+            pictureBoxspaghetti.Show();
+        }
+
+        //the purpose of the following code is to hide the image
+        private void btnHide_Click(object sender, EventArgs e)
+        {
+            pictureBoxspaghetti.Hide();
+            lblFavouritemeal.Text = "My favourite meal";
+        }
+
+        /*The purpose of the following code is to clear the textboxes and the answer label as well as
+         * to place the cursor in the first textbox.*/
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            textBoxnumber.Clear();
+            textBoxmultiplier.Clear();
+            textBoxnumber.Select();
+            lblAnswer.Text = " ";
+        }
+
+        /*The purpose of the following code is to hide the picture and to show the data label and 
+         * paragraphs about my topic which is spaghetti*/
+        private void btnData_Click(object sender, EventArgs e)
+        {
+            pictureBoxspaghetti.Hide();
+            string description = "My favourite meal";
+            lblFavouritemeal.Text = description + "\n" + "My topic is: " + description + ". My favourite meal is spaghetti. It is a meal I" +
+                " have always enjoyed eating as well as cooking. It is incredibly filling and, if you make it right, downright delicious. \n \n" +
+                "There are many ingredients you can put into spaghetti. For me some of my favourites are onions, garlic, mushrooms, spinach, broccoli, \n" +
+                " tomatoes, and bell peppers. \n \n There is also the matter of sauce. The sauce that you buy will determine much of the flavour." +
+                " A brand that I like to buy is Classico, though, I think it would be interesting to try to make my own sauce.";
+
+        }
+
+        //the purpose of the following code is to display a message box with information about the picture.
+        private void pictureBoxspaghetti_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("This is a picture of spaghetti.", "Jonah Martin");
+
+        }
+
+        //The purpose of the following code is to claculate and display an answer based on the two user inputs.
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            int number = Convert.ToInt32(textBoxnumber.Text);
+            int multiplier = Convert.ToInt32(textBoxmultiplier.Text);
+            int answer = number * multiplier;
+            lblAnswer.Text = answer.ToString();
+        }
+    }
+}
